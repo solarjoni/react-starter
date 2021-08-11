98546a21f47697eb34ec7251f92117c3a7171500
solarjoni <26577692+solarjoni@users.noreply.github.com>
2021-08-11 09:46:31 +0000

    final code

--- script.jsx
+++ script.jsx
@@ -1,4 +1,15 @@
+const name = "Mike"
+const now = new Date()
+const time = "The time is currently: " +  now.toLocaleString() 
+
+const getGreeting = () => {
+  if (now.getHours() < 12)
+    return <h1>Good morning, { name }! { time }</h1>
+  else
+    return <h1>Good afternoon, { name }! { time }</h1>
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  getGreeting(),
   document.getElementById('root')
-)
+) 
